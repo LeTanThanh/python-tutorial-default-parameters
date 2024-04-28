2853c31c0d5c63acb4b94a404a17a6b377ba9cf4
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-28 12:27:24 +0700

    Introduction to Python default parameters

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 if __name__ == "__main__":
-  print("Python Default Parameters")
+  # Introduction to Python default parameters
+
+  """
+  def function_name(param1, param2 = value2, param3 = value3, ...):
+  """
+
+  """
+  To use default parameters, you need to place parameters with the default values after other parameters.
+  Otherwise, you'll get a syntax error.
+
+  For example, you can not do something like this:
+
+  def function_name(param1 = value1, param2, param3):
+
+  This cause a synyax error
+  """
